10e471ad1e35b3997f86c1bcb4884df17ad8239a
Bruno <brn.pablox@gmail.com>
2025-12-30 21:48:26 -0300

    First

---
+++ src/main/java/dev/bruno/project_demand_management/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.bruno.project_demand_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bruno/project_demand_management/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bruno.project_demand_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
