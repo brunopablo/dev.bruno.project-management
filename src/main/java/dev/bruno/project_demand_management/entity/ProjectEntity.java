a6e9ec04ff8ad675b77d38a743c7010e895b55da
Bruno <brn.pablox@gmail.com>
2025-12-30 22:43:34 -0300

    Criado camada entity. Implementado parcialmente o mapeamento de entidades tasks e projects. Concluido relacionamento entre as entidades.

---
+++ src/main/java/dev/bruno/project_demand_management/entity/ProjectEntity.java
@@ -0,0 +1,98 @@
+package dev.bruno.project_demand_management.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_project")
+public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    
+    private String description;
+
+    private LocalDate startDate;
+    
+    private LocalDate endDate;
+
+    @OneToMany(mappedBy="projectEntity")
+    private List<TaskEntity> tasks;
+
+    public ProjectEntity() {
+    }
+
+    public ProjectEntity(
+            UUID id,
+            String name,
+            String description,
+            LocalDate startDate,
+            LocalDate endDate,
+            List<TaskEntity> tasks) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.tasks = tasks;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<TaskEntity> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskEntity> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/dev/bruno/project_demand_management/util/StatusEnum.java
@@ -0,0 +1,8 @@
+package dev.bruno.project_demand_management.util;
+
+
+public enum StatusEnum {
+    TODO,
+    DOING,
+    DONE
+}
